c03dbe4ec691c29d0b6e4cac48658667dc9aabdb
aayushi kambariya <aayushi.kambriya5@gmail.com>
2020-06-12 17:04:05 +0530

    Updates

---
+++ precache-manifest.6c3f9f7d189f857a83b07ef465f31301.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9445919a2c0d6f78d9bebd2b448fa29b",
+    "url": "/postalcode-map/index.html"
+  },
+  {
+    "revision": "99d9e7884c78e981fff5",
+    "url": "/postalcode-map/static/css/main.53c04d98.chunk.css"
+  },
+  {
+    "revision": "00db6f9b9e47d8603485",
+    "url": "/postalcode-map/static/js/2.38a2d9ff.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/postalcode-map/static/js/2.38a2d9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d9e7884c78e981fff5",
+    "url": "/postalcode-map/static/js/main.04af2ef0.chunk.js"
+  },
+  {
+    "revision": "fd44821202e4bbfa96a6",
+    "url": "/postalcode-map/static/js/runtime-main.eac39b17.js"
+  },
+  {
+    "revision": "6556553d78e93c49476368377f2d4916",
+    "url": "/postalcode-map/static/media/back.6556553d.svg"
+  },
+  {
+    "revision": "7709dbde34240099dc559ab8147874d5",
+    "url": "/postalcode-map/static/media/next.7709dbde.svg"
+  }
+]);
